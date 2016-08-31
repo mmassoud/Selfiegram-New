301a97110242de22ba49e2a459f92708ee97fd82
Michael Massoud <yl.massoud@gmail.com>
2016-08-30 21:47:59 -0400

    Initial Commit

---
+++ Selfiegram NewTests/Selfiegram_NewTests.swift
@@ -0,0 +1,36 @@
+//
+//  Selfiegram_NewTests.swift
+//  Selfiegram NewTests
+//
+//  Created by michael massoud on 2016-08-30.
+//  Copyright © 2016 michael massoud. All rights reserved.
+//
+
+import XCTest
+@testable import Selfiegram_New
+
+class Selfiegram_NewTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
