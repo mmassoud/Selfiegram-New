55b9737da5c68b472f8434dfb5e56a86cc749ab7
Michael Massoud <yl.massoud@gmail.com>
2016-09-20 20:46:53 -0400

    w5d1
    more parse

--- Selfiegram New/FeedViewController.swift
+++ Selfiegram New/FeedViewController.swift
@@ -7,9 +7,13 @@
 //
 
 import UIKit
+import Parse
 import Photos
 
 class FeedViewController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    
+
     
     @IBAction func cameraButtonPressed(sender: AnyObject) { // 1: Create an ImagePickerController
             let pickerController = UIImagePickerController()
@@ -63,6 +67,37 @@ class FeedViewController: UITableViewController, UIImagePickerControllerDelegate
 
     
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage {
+            
+            // setting the compression quality to 90%
+            if let imageData = UIImageJPEGRepresentation(image, 0.9),
+                let imageFile = PFFile(data: imageData),
+                let user = PFUser.currentUser(){
+                
+                //2. We create a Post object from the image
+                let post = Post(image: imageFile, user: user, comment: "A Selfie")
+                
+                post.saveInBackgroundWithBlock({ (success, error) -> Void in
+                    if success {
+                        print("Post successfully saved in Parse")
+                        
+                        //3. Add post to our posts array, chose index 0 so that it will be added
+                        //   to the top of your table instead of at the bottom (default behaviour)
+                        self.posts.insert(post, atIndex: 0)
+                        
+                        //4. Now that we have added a post, updating our table
+                        //   We are just inserting our new Post instead of reloading our whole tableView
+                        //   Both method would work, however, this gives us a cool animation for free
+                        
+                        let indexPath =  NSIndexPath(forRow: 0, inSection: 0)
+                        self.tableView.insertRowsAtIndexPaths([indexPath], withRowAnimation: .Automatic)
+                        
+                    }
+                })
+            }
+        }
+        
+        
         
         // 1. When the delegate method is returned, it passes along a dictionary called info.
         //    This dictionary contains multiple things that maybe useful to us.
@@ -130,99 +165,26 @@ class FeedViewController: UITableViewController, UIImagePickerControllerDelegate
     override func viewDidLoad() {
         super.viewDidLoad()
         
-//        let urlString: String = "https://www.flickr.com/services/rest/?method=flickr.photos.search&format=json&nojsoncallback=1&api_key=2a5fd21d2ccaf9e104f0f39eb928b636 &tags=cat"
-//        
-//        let urlString = "https://www.flickr.com/services/rest/?method=flickr.photos.search&format=json&nojsoncallback=1&api_key=2a5fd21d2ccaf9e104f0f39eb928b636&tags=cat"
-//        let url: NSURL = NSURL(string: urlString)!
-//        
-//        print("\(url)")
-        
-        let task = NSURLSession.sharedSession().dataTaskWithURL(NSURL(string: "https://www.flickr.com/services/rest/?method=flickr.photos.search&format=json&nojsoncallback=1&api_key=2a5fd21d2ccaf9e104f0f39eb928b636&tags=cat")!) { (data, response, error) -> Void in
-            
-            if let jsonUnformatted = try? NSJSONSerialization.JSONObjectWithData(data!, options: []),
-                let json = jsonUnformatted as? [String : AnyObject],
-                let photosDictionary = json["photos"] as? [String : AnyObject],
-                let photosArray = photosDictionary["photo"] as? [[String : AnyObject]]
-            {
+        if let query = Post.query() {
+           query.orderByDescending("createdAt")
+            query.includeKey("user")
+            query.findObjectsInBackgroundWithBlock({ (posts, error) -> Void in
                 
-                for photo in photosArray {
-                    
-                    if let farmID = photo["farm"] as? Int,
-                        let serverID = photo["server"] as? String,
-                        let photoID = photo["id"] as? String,
-                        let secret = photo["secret"] as? String {
-                        
-                        let photoURLString = "https://farm\(farmID).staticflickr.com/\(serverID)/\(photoID)_\(secret).jpg"
-                        if let photoURL = NSURL(string: photoURLString){
-                            let me = User(userName: "danny", profileImage: UIImage(named: "Grumpy-Cat")!)
-                            let post = Post(imageURL: photoURL, user: me, comment: "A Flickr Selfie")
-                            self.posts.append(post)
-                        }
-                    }
-                    
-                }
-                
-                // We use dispatch_async because we need update all UI elements on the main thread.
-                // This is a rule and you will see this again whenever you are updating UI.
-                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                if let posts = posts as? [Post]{
+                    self.posts = posts
                     self.tableView.reloadData()
-                })
-                
-            }else{
-                print("error with response data")
-            }
+                }
+               
+            })
             
         }
-        
-        task.resume()
-
-        
-//        let task = NSURLSession.sharedSession().dataTaskWithURL(url)
-//        { (data, response, error) -> Void in
-//            if let jsonUnformatted = try? NSJSONSerialization.JSONObjectWithData(data!, options: []),
-//                let json = jsonUnformatted as? [String : AnyObject],
-//                let photosDictionary = json["photos"] as? [String : AnyObject],
-//                let photosArray = photosDictionary["photo"] as? [[String : AnyObject]]
-//            {
-//                
-//                for photo in photosArray {
-//                    
-//                    if let farmID = photo["farm"] as? Int,
-//                        let serverID = photo["server"] as? String,
-//                        let photoID = photo["id"] as? String,
-//                        let secret = photo["secret"] as? String {
-//                        
-//                        let photoURLString = "https://farm\(farmID).staticflickr.com/\(serverID)/\(photoID)_\(secret).jpg"
-//                        print(photoURLString)
-//                        
-//                    }
-//                    
-//                }
-//            }else{
-//                print("error with response data")
-//            }
-//    }
-        
-        
-       
-        // UIImage has an initializer where you can pass in the name of an image in your project to create an UIImage
-        // UIImage(named: "Grumpy-Cat") can return nil if there is no image called "grumpy-cat" in your project
-        // Our definition of Post did not include the possibility of a nil UIImage
-        // so, therefore we have to add a ! at the end of it
-        
-
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem()
     }
+    
 
     override func didReceiveMemoryWarning()
     {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+        }
 
 
     // MARK: - Table view data source
@@ -242,39 +204,61 @@ class FeedViewController: UITableViewController, UIImagePickerControllerDelegate
     }
 
     
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell
-    {
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("postCell", forIndexPath: indexPath) as! SelfieCell
-        
         let post = self.posts[indexPath.row]
         
-        
         // I've added this line to prevent flickering of images
-        // We are inside the cellForRowAtIndexPath method that gets called everything we layout a cell
-        // Because we are reusing "postCell" cells, a reused cell might have an image already set on it.
+        // We are inside the cellForRowAtIndexPath method that gets called everything we lay out a cell
         // This always resets the image to blank, waits for the image to download, and then sets it
-        
         cell.selfieImageView.image = nil
         
-        let task = NSURLSession.sharedSession().downloadTaskWithURL(post.imageURL) { (url, response, error) -> Void in
-            
-            if let imageURL = url,
-                let imageData = NSData(contentsOfURL: imageURL){
-                dispatch_async(dispatch_get_main_queue(), { () -> Void in
-                    
-                    cell.selfieImageView.image = UIImage(data: imageData)
-                    
-                })
+        let imageFile = post.image
+        imageFile.getDataInBackgroundWithBlock { (data, error) -> Void in
+            if let data = data {
+                let image = UIImage(data: data)
+                cell.selfieImageView.image = image
             }
         }
         
-        task.resume()
-        
-//        cell.usernameLabel.text = post.user.userName
+        cell.usernameLabel.text = post.user.username
         cell.commentLabel.text = post.comment
         
         return cell
-    }        // Configure the cell...
+    }
+        
+    
+        
+        
+//        let post = self.posts[indexPath.row]
+//        
+//        
+//        // I've added this line to prevent flickering of images
+//        // We are inside the cellForRowAtIndexPath method that gets called everything we layout a cell
+//        // Because we are reusing "postCell" cells, a reused cell might have an image already set on it.
+//        // This always resets the image to blank, waits for the image to download, and then sets it
+//        
+//        cell.selfieImageView.image = nil
+//        
+//        let task = NSURLSession.sharedSession().downloadTaskWithURL(post.imageURL) { (url, response, error) -> Void in
+//            
+//            if let imageURL = url,
+//                let imageData = NSData(contentsOfURL: imageURL){
+//                dispatch_async(dispatch_get_main_queue(), { () -> Void in
+//                    
+//                    cell.selfieImageView.image = UIImage(data: imageData)
+//                    
+//                })
+//            }
+//        }
+//        
+//        task.resume()
+//        
+////        cell.usernameLabel.text = post.user.userName
+//        cell.commentLabel.text = post.comment
+//        
+//        return cell
+//    }        // Configure the cell...
         
         
 
@@ -309,13 +293,6 @@ class FeedViewController: UITableViewController, UIImagePickerControllerDelegate
     */
 
     /*
-    // Override to support rearranging the table view.
-    override func tableView(tableView: UITableView, moveRowAtIndexPath fromIndexPath: NSIndexPath, toIndexPath: NSIndexPath) {
-
-    }
-    */
-
-    /*
     // Override to support conditional rearranging of the table view.
     override func tableView(tableView: UITableView, canMoveRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         // Return false if you do not want the item to be re-orderable.
@@ -335,3 +312,7 @@ class FeedViewController: UITableViewController, UIImagePickerControllerDelegate
 
 
 }
+
+
+    
+
--- Selfiegram New/Post.swift
+++ Selfiegram New/Post.swift
@@ -7,27 +7,49 @@
 //
 
 import Foundation
-
+import Parse
 import UIKit
 
-class Post {
+class Post:PFObject, PFSubclassing {
     
-    let imageURL:NSURL
-    let user:User
-    let comment:String
+    @NSManaged var image:PFFile
+    @NSManaged var user:PFUser
+    @NSManaged var comment:String
     
-    init(imageURL:NSURL, user:User, comment:String){
+    static func parseClassName() -> String {
+        return "Post"
+    }
+    
+    // convenience init method, because it’s building on top of PFObject’s init, rather than overriding it.
+    convenience init(image:PFFile, user:PFUser, comment:String){
         // You can name the property you are passing into the function the
         // same name as the class' property. To distinguish the two
         // add "self." to the beginning of the class' property.
-        // So for example we are passing in an NSURL called imageURL and setting it as our
-        // imageURL property for Post.
-        self.imageURL = imageURL
+        self.init()
+        self.image = image
         self.user = user
         self.comment = comment
     }
     
+    
+    
 }
+//    let imageURL:NSURL
+//    let user:User
+//    let comment:String
+//    
+//    init(imageURL:NSURL, user:User, comment:String){
+//        // You can name the property you are passing into the function the
+//        // same name as the class' property. To distinguish the two
+//        // add "self." to the beginning of the class' property.
+//        // So for example we are passing in an NSURL called imageURL and setting it as our
+//        // imageURL property for Post.
+//        self.imageURL = imageURL
+//        self.user = user
+//        self.comment = comment
+//    }
+    
+
     
     
     
